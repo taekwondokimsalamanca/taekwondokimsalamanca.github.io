a5249d9e3677725d51d224e3f7f1dbd6db6b33ba
taekwondokimsalamanca <140203267+taekwondokimsalamanca@users.noreply.github.com>
2023-07-25 22:50:48 +0200

    Formato Mejorado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-var imagen_seleccionada = 1;
+/* Script desarrollado por Javier LOpez Sanchez */
+/**********************************************************************/
+
+/**********************************************************************/
+/* Variables globales */
+var imagen_seleccionada = 1		; // Imagen en pantalla
+var timeout_imagen		= 3000	; // Timeout de la imagen (milisegundos)
+/**********************************************************************/
 
 function inicializarPagina()
 {
@@ -10,26 +17,36 @@ function inicializarPagina()
 function cambiarImagen()
 {
 
-    var imagen_fondo = document.getElementsByClassName("fondo")[0].getElementsByTagName("img")[0];
+    var imagen_fondo = document.getElementsByClassName("fondo")[0].getElementsByTagName("img")[0]; // ObtenciOn de la imagen
+
+    imagen_fondo.style.opacity = "0.25%"; // Final Fade In
+
+	// Inicio Fade Out
     imagen_fondo.classList.remove("imagenFadeIn");
     imagen_fondo.classList.add("imagenFadeOut");
 
-    setTimeout(
-        function()
+    setTimeout( function() // Espera Fade Out
         {
-            imagen_fondo.classList.remove("imagenFadeOut");
-            imagen_fondo.classList.add("imagenFadeIn");
-            imagenActual(imagen_fondo);
-            setTimeout(function() {cambiarImagen();}, 3000)
-        }, 3000)
+
+            imagen_fondo.style.opacity = "0%"; // Final Fade Out
+
+			// Cambio de Imagen + Inicio Fade In
+			imagen_fondo.classList.remove("imagenFadeOut");
+			imagenActual(imagen_fondo);
+			imagen_fondo.classList.add("imagenFadeIn");
+
+            setTimeout(function() {cambiarImagen();}, timeout_imagen);  // Espera Fade In
+
+        }, timeout_imagen)
 
 }
 
 function imagenActual(imagen_fondo)
 {
 
-         if(imagen_seleccionada == 1) { imagen_fondo.src = "img/InauguracionYopChagui.jpg"; imagen_seleccionada = 2; }
-    else if(imagen_seleccionada == 2) { imagen_fondo.src = "img/FotoGrupal5.jpg"; imagen_seleccionada = 3; }
-    else if(imagen_seleccionada == 3) { imagen_fondo.src = "img/InauguracionInicioCombate.jpg"; imagen_seleccionada = 1; }
+    if(imagen_seleccionada == 3) imagen_seleccionada = 1;
+    else imagen_seleccionada++;
 
+    imagen_fondo.src = "img/FotoPortada" + imagen_seleccionada + ".jpg";
+    
 }
